31980c06c38206053e59891674e36c9dc8e97462
NazarDunets <jcq.nazardunets@gmail.com>
2020-12-06 13:23:38 +0200

    Done!

---
+++ Conventions/Comparison/src/Task.kt
@@ -0,0 +1,12 @@
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        if (year != other.year) return year - other.year
+        if (month != other.month) return month - other.month
+        return dayOfMonth - other.dayOfMonth
+    }
+}
+
+fun test(date1: MyDate, date2: MyDate) {
+    // this code should compile:
+    println(date1 < date2)
+}
